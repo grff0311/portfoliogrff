29ff734aba33e96cc2eb6dbf3bf4cc061a2f72ad
Griffin Roleh <griffinroleh24113@gmail.com>
2024-11-19 21:30:12 +0700

    mengubah icon

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,8 +1,10 @@
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 
-import { RiNextjsFill } from "react-icons/ri";
-import { FaVuejs } from "react-icons/fa";
+import { TbHexagonNumber4Filled } from "react-icons/tb";
+import { TbHexagonNumber3Filled } from "react-icons/tb";
+import { TbHexagonNumber2Filled } from "react-icons/tb";
+import { TbHexagonNumber1Filled } from "react-icons/tb";
 
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
@@ -19,7 +21,7 @@ function Experience() {
                     className="vertical-timeline-element--work"
                     date="2023 - present"
                     iconStyle={{ background: '#222831', color: '#fff' }}
-                    icon={<RiNextjsFill />}
+                    icon={<TbHexagonNumber1Filled />}
                 >
                     <h3 className="vertical-timeline-element-title">Art Director</h3>
                     <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
@@ -31,7 +33,7 @@ function Experience() {
                     className="vertical-timeline-element--work"
                     date="2022 - 2023"
                     iconStyle={{ background: '#222831', color: '#fff' }}
-                    icon={<FaVuejs />}
+                    icon={<TbHexagonNumber2Filled />}
                 >
                     <h3 className="vertical-timeline-element-title">Art Director</h3>
                     <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
@@ -43,7 +45,19 @@ function Experience() {
                     className="vertical-timeline-element--work"
                     date="2022 - 2023"
                     iconStyle={{ background: '#222831', color: '#fff' }}
-                    icon={<FaVuejs />}
+                    icon={<TbHexagonNumber3Filled />}
+                >
+                    <h3 className="vertical-timeline-element-title">Art Director</h3>
+                    <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+                    <p>
+                    Creative Direction, User Experience, Visual Design, SEO, Online Marketing
+                    </p>
+                </VerticalTimelineElement>
+                <VerticalTimelineElement
+                    className="vertical-timeline-element--work"
+                    date="2022 - 2023"
+                    iconStyle={{ background: '#222831', color: '#fff' }}
+                    icon={<TbHexagonNumber4Filled />}
                 >
                     <h3 className="vertical-timeline-element-title">Art Director</h3>
                     <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
